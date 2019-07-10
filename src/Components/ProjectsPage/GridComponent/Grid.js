6ef33d286440ba052c71d52e85c00cb32c53a8c0
kunnu01 <kunalbhatia2409@gmail.com>
2019-07-10 16:41:06 +0530

    Add project page

--- src/Components/ProjectsPage/GridComponent/Grid.js
+++ src/Components/ProjectsPage/GridComponent/Grid.js
@@ -8,7 +8,6 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 const styles = theme => ({
     root: {
         display: 'flex',
-        // flexWrap: 'wrap',
         marginTop: '5%',
         justifyContent: 'space-around',
         overflow: 'hidden',
@@ -16,81 +15,93 @@ const styles = theme => ({
     gridList: {
         display: 'flex',
         justifyContent: 'space-between',
-        width: '90%',
+        width: '85vw',
         height: 'auto',
-        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
         transform: 'translateZ(0)',
+        [theme.breakpoints.down('sm')]: {
+            width: '100vw',
+        },
     },
     titleBar: {
         background:
         'linear-gradient(to bottom, rgba(0,0,0,0.5) 0%, ' +
         'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0.3) 100%)',
+        [theme.breakpoints.down('sm')]: {
+          height: '40%',
+        },
     },
     icon: {
         color: 'white',
     },
     projects: {
         cursor: 'pointer',
-        width: '200px',
+        width: '100%',
         height: 'auto',
-    }
+        [theme.breakpoints.down('sm')]: {
+          height: '20%',
+        },
+    },
 });
 
 const tileData = [
     {
+        img: './Images/Burger.png',
+        title: 'Burger App',
+        link: 'https://github.com/Kunnu01/burger-app',
+    },
+    {
         img: './Images/geekoconnector.JPG',
-        title: 'GeekoConnector (MERN Stack)',
-        author: 'author',
-        featured: false,
+        title: 'GeekoConnector',
+        link: 'https://github.com/Kunnu01/GeekoConnector',
+    },
+    {
+        img: './Images/Snake.png',
+        title: 'Snake Game',
+        link: 'https://github.com/Kunnu01/snake-game',
     },
     {
         img: './Images/colorGame.JPG',
-        title: 'Color Game (JavaScript)',
-        author: 'author',
-        featured: false,
+        title: 'Color Game',
+        link: 'https://github.com/Kunnu01/Color-Game',
     },
     {
-      img: './Images/weatherApp.JPG',
-      title: 'Weather App (Node.js)',
-      author: 'author',
-      featured: false,
-  },
-  {
-    img: './Images/noteApp.JPG',
-    title: 'Notes App (Node.js)',
-    author: 'author',
-    featured: false,
-  },
-  {
-    img: './Images/patatp.png',
-    title: 'Patatap Clone (JavaScript)',
-    author: 'author',
-    featured: false,
-  },
-  {
-      img: './Images/mySketched.JPG',
-      title: 'My Sketches (JavaScript)',
-      author: 'author',
-      featured: false,
-  },
-  {
+        img: './Images/weatherApp.JPG',
+        title: 'Weather App',
+        link: 'https://github.com/Kunnu01/Weather-App',
+    },
+    {
+        img: './Images/noteApp.JPG',
+        title: 'Notes App',
+        link: 'https://github.com/Kunnu01/Node-Note-App',
+    },
+    {
+        img: './Images/patatp.png',
+        title: 'Patatap Clone',
+        link: 'https://github.com/Kunnu01/patatap_clone',
+    },
+    {
+        img: './Images/mySketched.JPG',
+        title: 'My Sketches',
+        link: 'https://github.com/Kunnu01/sketch_and_code',
+    },
+    {
         img: './Images/todoapp.JPG',
-        title: 'Todo App (JavaScript)',
-        author: 'author',
-        featured: false,
+        title: 'Todo App',
+        link: 'https://github.com/Kunnu01/ToDo-List-App',
     },
 ];
+
 function AdvancedGridList(props) {
   const { classes } = props;
 
   return (
     <div className={classes.root}>
-      <GridList cellHeight={270} spacing={24} className={classes.gridList}>
+      <GridList cellHeight="auto" cols={2} spacing={24} className={classes.gridList}>
         {tileData.map(tile => (
-          <GridListTile className={classes.projects} key={tile.img} cols={tile.featured ? 3 : 0.6} rows={tile.featured ? 3 : 1}>
+          <GridListTile component="a" href={tile.link} target="_blank" col="1" className={classes.projects}  key={tile.img}>
             <img
               src={tile.img}
-              style={{objectFit: 'inherit', height: '100%', width: '100%', borderRadius: '3%'}} 
+              style={{height: '100%', width: '100%', borderRadius: '3%'}} 
               alt={tile.title} 
               />
             <GridListTileBar
